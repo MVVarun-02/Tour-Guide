7f261eb41cb74cf3f95dc918fc6ff368f834b67a
MVVarun-02 <94582488+MVVarun-02@users.noreply.github.com>
2023-09-03 02:16:29 +0530

    Add files via upload

---
+++ Tour Guide/java/com/example/tour/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.tour;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+
+import com.google.android.material.tabs.TabLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Find the view pager that will allow the user to swipe between fragments
+        ViewPager viewPager = findViewById(R.id.viewpager);
+
+        // Create an adapter that knows which fragment should be shown on each page
+        SimpleFragmentPagerAdapter adapter = new SimpleFragmentPagerAdapter(MainActivity.this, getSupportFragmentManager());
+
+        viewPager.setAdapter(adapter);
+        viewPager.setOffscreenPageLimit(1);
+
+        // Give the TabLayout the ViewPager
+        TabLayout tabLayout = findViewById(R.id.sliding_tabs);
+        tabLayout.setupWithViewPager(viewPager);
+    }
+}
